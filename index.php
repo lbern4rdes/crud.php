d1cff2fb7db655d82283e95a76d99a7503f73b3f
lbern4rdes <luisgbragabernardes@gmail.com>
2024-07-25 09:58:05 -0300

    Mudanças na index

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ include_once "conexao.php";
 try {
   //execução da instrução
   $consulta = $conectar->query("SELECT * FROM usuario");
-
+  $consulta->execute();
+  // var_dump($consulta->execute());
   echo "<a href = 'formCadastro.php'>Novo Cadastro<a/><br><br>Listagem de Usuários";
   echo "<table border='1'><tr><td>ID</td><td>NOME</td><td>CPF</td><td>TELEFONE</td><td>Ações</td></tr>";
 
@@ -19,34 +20,3 @@ try {
     echo $e->getMessage();
 }
 
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tabela de usuários</title>
-</head>
-<body>
-  <table border='1'>
-    <tr> 
-      <td>ID</td>
-      <td>NOME</td>
-      <td>CPF</td>
-      <td>TELEFONE</td>
-      <td>Ações</td>
-    </tr>
-    
-    <?php while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) { var_dump($linha); ?>
-    vard
-    <tr> 
-      <td>ID</td>
-      <td><?= $linha["nome"];?></td>
-      <td>CPF</td>
-      <td>TELEFONE</td>
-      <td>Ações</td>
-    </tr>
-    <?php }?>
-  </table>
-</body>
-</html>
